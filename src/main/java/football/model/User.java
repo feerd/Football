815f5d224b4112e3a0722a5b72882eec1ae04345
bzz <feerd@bzz.ch>
2021-03-04 12:11:19 +0100

    user implemented

--- src/main/java/football/model/User.java
+++ src/main/java/football/model/User.java
@@ -1,4 +1,15 @@
 package football.model;
 
 public class User {
+    private String playerUUID;
+    private String userName;
+    private String password;
+
+    public void logon() {
+
+    }
+
+    public void logoff() {
+
+    }
 }
